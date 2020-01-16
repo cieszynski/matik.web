7e5f8645aeba66fd049f852550002db5d400755c
cieszynski <admin@cieszysnki.de>
2020-01-16 22:17:28 +0100

    initial commit

---
+++ sw.js
@@ -0,0 +1,12 @@
+/* sw.js */
+self.addEventListener('install', (event) => {
+    console.log('install');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('activate');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('fetch');
+});
